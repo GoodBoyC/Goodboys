d6ad3aeed99e4b71399f4d50db9001dec3eb14bd
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2022-01-25 11:06:19 +0100

    Update goodboys.py

--- goodboys.py
+++ goodboys.py
@@ -43,7 +43,7 @@ loc = requests.get('https://ipapi.com/ip_api.php?ip=' + ip, headers={'Referer': 
 def linex():
 	os.system('echo  "\n ======================================\n" | lolcat -a -d 2 -s 50')
 def logo():
-	os.system('echo "\n      ╔═════════════════════════════╗\n    ║ TOOL NAME: { GOODBOYS }  ║\n    ║ AUTHOR   : MR. CHINDA    ║\n    ║ GITHUB   : github.com/GoodBoyC     \n    ╚═════════════════════════════╝" | lolcat -a -d 2 -s 50')	
+	os.system('echo "\n    ╔═════════════════════════════╗\n    ║ TOOL NAME: { GOODBOYS }     ║\n    ║ AUTHOR   : MR. CHINDA       ║\n    ║ GITHUB   : github.com/GoodBoyC     \n    ╚═════════════════════════════╝" | lolcat -a -d 2 -s 50')	
 
 def main():
 	os.system("clear")
